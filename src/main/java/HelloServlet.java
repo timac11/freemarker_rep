dd86510ba0c632e3803ff4ec7decee97100ad7bb
timac11 <timac11@bk.ru>
2016-07-19 16:58:47 +0300

    the_first_commit

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,48 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private static List<User> userList = new ArrayList<User>();
+	
+	static {
+		userList.add(new User("Bill", "Gates"));
+		userList.add(new User("Steve", "Jobs"));
+		userList.add(new User("Larry", "Page"));
+		userList.add(new User("Sergey", "Brin"));
+		userList.add(new User("Larry", "Ellison"));
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setAttribute("users", userList);
+		request.getRequestDispatcher("/index.ftl").forward(request, response);
+
+	}
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+		
+		if(null != firstname && null != lastname
+				&& !firstname.isEmpty() && !lastname.isEmpty()) {
+			
+			synchronized (userList) {
+				userList.add(new User(firstname, lastname));
+			}
+			
+		}
+		
+		doGet(request, response);
+	}
+}
